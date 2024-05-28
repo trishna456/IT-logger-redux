4712cd66f4e0938df722cac15f4de980baad9156
Trishna <trishna.patil13@gmail.com>
2024-05-28 16:51:12 -0400

    added preloader, logitems

---
+++ src/components/layout/Preloader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Preloader(props) {
+  return (
+    <div className="progress blue lighten-4">
+      <div className="indeterminate blue"></div>
+    </div>
+  );
+}
+
+export default Preloader;
