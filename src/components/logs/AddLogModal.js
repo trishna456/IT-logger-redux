b5727eeb73165b37c713e224d7bb1222fb969bb7
Trishna <trishna.patil13@gmail.com>
2024-05-31 03:20:04 -0400

    tech select options component added

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import TechSelectOptions from "../techs/TechSelectOptions";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { addLog } from "../../actions/logActions";
@@ -61,9 +62,7 @@ const AddLogModal = ({ addLog }) => {
               <option value="" disabled>
                 Select Technician
               </option>
-              <option value="Sam Smith">Sam Smith</option>
-              <option value="John Doe">John Doe</option>
-              <option value="Sara Wilson">Sara Wilson</option>
+              <TechSelectOptions />
             </select>
           </div>
         </div>
