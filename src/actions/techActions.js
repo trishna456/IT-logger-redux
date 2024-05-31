6d88cff355a5eb0348d9defe6a26b4b7d28d6f2f
Trishna <trishna.patil13@gmail.com>
2024-05-31 03:27:45 -0400

    adding tech functionality

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -25,6 +25,32 @@ export const getTechs = () => async (dispatch) => {
   }
 };
 
+//add techs to server
+export const addTech = (tech) => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch("/techs", {
+      method: "POST",
+      body: JSON.stringify(tech),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_TECH,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: error.response.statusText,
+    });
+  }
+};
+
 //set loading to true
 export const setLoading = () => {
   return {
